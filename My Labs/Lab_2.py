d31cf7fa551f7fede276281e130df8615794097c
www8351 <www8351@gmail.com>
2022-05-05 04:33:15 +0300

    Basic

--- My Labs/Lab_2.py
+++ My Labs/Lab_2.py
@@ -8,14 +8,12 @@ from random import randint
 from time import sleep
 
 while(True):
-    choice=input("Menu:\n--------------\n1.printing 100 numbers\n2.check fibo\n3.randint numbers until we get 12 or we count 10 times\n")
-########################################################
-    if(choice=="1"):
+    choice=input("Menu:\n--------------\n1.Zero To 100\n2.Fibo\n3.randint_Lucky_Num 12 or count 10 Games\n")
+    if(choice == "1"):
         for i in range(1,101):
             print(i)
-########################################################
-    elif(choice=="2"):
-        #fibo = [1, 2, 3, 5, 8, 13, 21]
+    elif(choice == "2"):
+        #fibo = [ 2, 3, 5, 8, 13, 21, 34]
         fibo=[]
         for i in range(7):
             fibo.append(int(input("Enter a number: ")))
@@ -29,21 +27,20 @@ while(True):
                 boolean = "False"
                 break
         if boolean == "True":
-            print("It is fibo series")
+            print("It is Fibo series")
         else:
             print("It isn't fibo series")
-########################################################
-    elif(choice=="3"):
-        counter = 1
-        num=randint(1,12)
-        while(num!=12 and counter<11):
-            print("Counter:" + str(counter) + "  Number:" + str(num) + "\n")
-            counter=counter+1
-            num = randint(1, 12)
+    elif(choice == "3"):
+        x = 1
+        y = randint(1,12)
+        while(y!=12 and x<11):
+            print("Counter:" + str(x) + "  Number:" + str(y) + "\n")
+            x = x+1
+            y = randint(1, 12)
     else:
         print("Enter 1-3 only!!\n")
         continue
-    exit=input("Do you want to exit? yes/no\n")
-    if(exit=="yes"):
+    exit = input("Do you want to exit? yes/no\n")
+    if(exit == "yes"):
         print("Thank you and bye bye...\n")
         break
